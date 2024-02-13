041ee1c5f31dca5990e75e9bab4a253ccd04c176
田中泰申 <k022g0122@g.neec.ac.jp>
2024-02-13 17:14:05 +0900

    05_01

--- main.cpp
+++ main.cpp
@@ -5,30 +5,6 @@
 
 using namespace std;
 int main() {
-//	vector<int> b;
-//	b.push_back(40);
-//	b.pop_back();
-//	//system("pause");
-//
-//	vector<float> c = { 0.0f,1.0f,2.0f };
-//	printf(" %f", c[1]);
-//	c[1] = -1.5f;
-//	printf("%f", c[1]);
-//
-//	vector<int> d;
-//	d.resize(10);
-//	int size = d.size();
-//	for (int i = 0; i < size; i++) {
-//		printf("%d", d[1]);
-//	}
-//
-//	vector<int> e = { 1,2,3 };
-//	vector<int>::iterator it;
-//	it = e.begin();
-//	cout << *it << endl;
-//	it++;
-//	cout << *it << endl;
-
 	std::list<string> Yamanote_Sen
 	{ "\nTokyo","Takada","Akihabara","Okachimachi","Ueno",
 		"Uguisudani","Nippori",/*"Nishi-Nippori",*/"Tabata","Komagome",
@@ -64,22 +40,6 @@ int main() {
 
 		cout << *it_Yamanote_Sen << endl;
 	}
-	/*std::list<char> list{ 1,3,5,7,10 };*/
-
-	//vector<int> g = { 1,2,3 };
-	//g.erase(g.begin() + 1);
-	//g.pop_back();
-
-	//std::list<int> list{1,3,5,7,10};
-		
-	//for (auto itr = lst.begin(); itr != list.end(); ++itr) {
-	//	std::cout << *itr << "\n";
-	//}
-
-	//for (std::list<int>::iterator itr = list.begin(); itr != list.end(); ++itr) {
-	//	itr = list.insert(itr, 0);
-	//	++itr;
-	//}
 
 	return 0;
 }
